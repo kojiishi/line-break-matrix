937d8ce4d068918fed7d196c3e292b4894d66efd
Koji Ishii <kojii@chromium.org>
2015-10-14 23:11:16 +0900

    line-break.html

---
+++ resources/ahem.js
@@ -0,0 +1,25 @@
+var ahem = function () {
+  var url = "resources/AHEM____.TTF";
+  if (window.FontFace) {
+    var font = new FontFace("Ahem", "url(" + url + ")");
+    document.fonts.add(font);
+    return font.load();
+  }
+
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode("@font-face{" +
+    "font-family:Ahem;" +
+    "src:url(" + url + ");}"));
+  document.getElementsByTagName('head')[0].appendChild(style);
+
+  if (document.fonts) {
+    var ready = document.fonts.ready;
+    if (ready)
+      return ready;
+  }
+
+  console.log("Cannot determine fonts status, wait for 1sec.")
+  return Promise(function (resolve, reject) {
+    setTimeout(resolve, 1000);
+  });
+}
